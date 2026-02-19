5e6fef855afcbc2a253662dc808d0c8500a21669
OblivionKingX <oblivionkingx228@gmail.com>
2026-02-19 12:55:25 +0100

    Initial website publish

---
+++ firebase-config.js
@@ -0,0 +1,43 @@
+// Firebase Configuration for TitanNetwork (Forum & Home)
+export const titanFirebaseConfig = {
+    apiKey: "AIzaSyB3FVgVVOAkxXCWUrRJZBdV03jS1KiCcn8",
+    authDomain: "titannetwork-27026.firebaseapp.com",
+    databaseURL: "https://titannetwork-27026-default-rtdb.firebaseio.com",
+    projectId: "titannetwork-27026",
+    storageBucket: "titannetwork-27026.firebasestorage.app",
+    messagingSenderId: "108826305543",
+    appId: "1:108826305543:web:86f640d23ed3a1859836a2",
+    measurementId: "G-6CZETCVJ1R"
+};
+
+// Firebase Configuration for SupremeMC (Media & Report Applications)
+export const supremeFirebaseConfig = {
+    apiKey: "AIzaSyC5lctDSba3oKtByBT51aDC5LSYd1J3c6s",
+    authDomain: "suprememc-help.firebaseapp.com",
+    databaseURL: "https://suprememc-help-default-rtdb.firebaseio.com",
+    projectId: "suprememc-help",
+    storageBucket: "suprememc-help.firebasestorage.app",
+    messagingSenderId: "602328746603",
+    appId: "1:602328746603:web:5f99161981a50c3a17ff2e",
+    measurementId: "G-2JJEPHRTCV"
+};
+
+export const ADMIN_EMAILS = [
+    "oblivionkingx228@gmail.com",
+    "kanielpt038@gmail.com",
+    "ahmadsangi234@gmail.com",
+    "tadejmuhic8@gmail.com",
+    "blowieknetwork@gmail.com"
+];
+
+export const OWNER_EMAIL = "oblivionkingx228@gmail.com";
+
+export const WEBHOOKS = {
+    MEDIA_APP: "https://discord.com/api/webhooks/1432748547802923201/CfHTSKNlSJ0f1aG5wFFKSvznL9h9DKZhx0tfMqzvyMtehgCspiUDQdZmkKO2FNiYPcnI",
+    REPORT_PLAYER: "https://discord.com/api/webhooks/1424384238496780371/XzrExbuwWSyEjXfVT0uwLaY7fqazlc8Az3u4gQ7iW3XJx0oLZNjAv5K79mkDPO_GCf9M",
+    STAFF_APPLY: "https://discord.com/api/webhooks/1424094998294696057/-RlwVFQnpJRk9RVY0DDkyc_bKuS_YtiYd-bMsElWhc_IfLl-ZYa--WHVhf8LQtpGuOrn",
+    UNBAN_APPEAL: "https://discord.com/api/webhooks/1424107775893704846/X7PTlu3Q6002ZXJ6371m-g4CRkjsqR05wFErUGfcTo1GDQ-8XqbnXD_LeBAsJafx85ia"
+};
+
+// Aliases for backward compatibility if needed
+export const firebaseConfig = titanFirebaseConfig;
